f4535d61b02ff94e98a10f9b87d1df9c170bc13a
ivan3123708 <ivan3123708@gmail.com>
2018-03-31 03:24:35 +0200

    Send messages only inside of room

--- public/js/chat.js
+++ public/js/chat.js
@@ -73,7 +73,6 @@ $('#message-form').on('submit', function(e) {
   e.preventDefault();
  
   socket.emit('createMessage', {
-    from: 'User',
     text: $('[name=message]').val()
   }, function() {
     $('[name=message]').val('');
--- server/server.js
+++ server/server.js
@@ -35,14 +35,21 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('Created message', message);
+    const user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
     callback('Message sent');
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.lat, coords.lng));
+    const user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.lat, coords.lng));
+    }
   });
 
   socket.on('disconnect', () => {
